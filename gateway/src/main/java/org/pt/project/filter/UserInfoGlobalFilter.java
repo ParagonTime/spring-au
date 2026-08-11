d7d8733793f11c6e205bf61969a279362b1e0ede
ParagonTime <fonmaterhorn@yandex.ru>
2026-08-11 13:51:52 +0300

    local kind 2

--- gateway/src/main/java/org/pt/project/filter/UserInfoGlobalFilter.java
+++ gateway/src/main/java/org/pt/project/filter/UserInfoGlobalFilter.java
@@ -20,6 +20,7 @@ import java.util.Map;
 @Component
 public class UserInfoGlobalFilter implements GlobalFilter {
 
+    // comment
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuerUri;
 
