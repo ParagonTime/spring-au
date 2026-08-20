3c8faae4a9d7f37192e9c74f269790f357c94cac
ParagonTime <fonmaterhorn@yandex.ru>
2026-08-20 21:47:29 +0300

    fix

--- gateway/src/main/java/org/pt/project/GatewayApp.java
+++ gateway/src/main/java/org/pt/project/GatewayApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApp { //1
+public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
     }
